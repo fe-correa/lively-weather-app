e3e85405931d4253b8c8e796fdcc75d8ed316404
fe-correa <feqscorrea@gmail.com>
2023-06-02 00:08:25 +0100

    Updated Forecast

--- src/app.js
+++ src/app.js
@@ -21,29 +21,47 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDate();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
-            <div class="weather-forecast-date">${day}.</div>
+            <div class="weather-forecast-date">${formatDay(forecastDay.date)}
+              </div>
                 <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-night.png"
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                    forecastDay.condition.icon
+                  }.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecast-temp">
-                  <span class="weather-forecast-temp-max">21º</span>
-                  <span class="weather-forecast-temp-min">15º</span>
+                  <span class="weather-forecast-temp-min">${Math.round(
+                    forecastDay.temperature.minimum
+                  )}º</span>
+                  <span class="weather-forecast-temp-max">${Math.round(
+                    forecastDay.temperature.maximum
+                  )} º</span>
             </div>
         </div>
               
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
